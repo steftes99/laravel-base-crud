41e70bd6d80134311cc244e381d36722b1e8ff76
steftes99 <stefanotescione83@gmail.com>
2021-09-10 17:58:50 +0200

    progressi

---
+++ laravel-base-crud/app/Comic.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    protected $table = 'comics';
+    protected $fillable = [
+        'title',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'sale_date',
+        'type'
+    ];
+}
