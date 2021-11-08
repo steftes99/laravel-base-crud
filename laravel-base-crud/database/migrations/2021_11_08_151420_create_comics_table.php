7235d8ddcb427038d0eea38ca2b18cd983e376be
steftes99 <stefanotescione83@gmail.com>
2021-11-08 16:42:44 +0100

    creazione db e model-controller

---
+++ laravel-base-crud/database/migrations/2021_11_08_151420_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('editor');
+            $table->string('artist');
+            $table->string('writer');
+            $table->tinyInteger('price');
+            $table->text('cover_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
